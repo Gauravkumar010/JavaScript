56e422a0580f365a788215717660e930dd5ec903
Gauravkumar010 <134727092+Gauravkumar010@users.noreply.github.com>
2023-12-10 10:57:23 +0000

    comparison

---
+++ basicjavascript/conversion.js
@@ -0,0 +1,73 @@
+let score = "33";
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score);
+
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber)
+
+//"33"=>33
+//"33qwe"=>NaN
+//true=>1 //false=>0
+
+let isLogged = "gaurav"
+let booleanIsloggedIn = Boolean(isLogged);
+//console.log(booleanIsloggedIn)
+
+//1=>true // 0=>false
+//""=>false
+// "gaurav"=>true
+
+
+let someNumber = 33;
+let stringNumber = String(someNumber)
+
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+
+//**********************Operation****************************/
+
+let value = 9
+let negValue = -value
+// console.log(negValue);
+
+
+let str1 = "Hello";
+let str2 = " Gaurav";
+
+// console.log(str1 + str2);
+
+// console.log("1"+2)
+// console.log(1+"2")
+// console.log("1"+2+2)
+// console.log(1+2+"2")
+
+// console.log((3+4)*5%2)
+
+// console.log(+true)
+// console.log(+"")
+
+let game = 100
+game++;
+console.log(game)
+
+
+
+//************Comparison********/
+
+
+
+// console.log(null>0)
+// console.log(null==0)
+// console.log(null>=0)
+
+// console.log(undefined>0)
+// console.log(undefined==0)
+// console.log(undefined>=0)
+
+console.log("2"==2)
+
+console.log("2"===2)
