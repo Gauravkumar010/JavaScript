79ced292561e1d21dfbf285fb32342400040ddd6
Gauravkumar010 <134727092+Gauravkumar010@users.noreply.github.com>
2023-12-10 06:43:42 +0000

    variable

---
+++ basicjavascript/basicVariable.js
@@ -0,0 +1,14 @@
+const accountId=12345
+let accountEmail="gaurav123@gmail.com";
+var accountPassword = "1456";
+accountCity = "sultanpur";
+
+// accountId=1456;//not allowed
+
+accountEmail="gkgk@gmail.com";
+accountPassword="789456";
+accountCity="azamgarh";
+
+console.log(accountId)
+
+console.table([accountId,accountEmail,accountPassword,accountCity]);
