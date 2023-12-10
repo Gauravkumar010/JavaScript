776f61a4d8bc9fdbd40f23b53160f8c2d92c01f0
Gauravkumar010 <134727092+Gauravkumar010@users.noreply.github.com>
2023-12-10 09:52:51 +0000

    datatype

---
+++ basicjavascript/datatype.js
@@ -0,0 +1,19 @@
+"Use strict";
+
+console.log(3+30);
+
+console.log("Gaurav")
+
+let age = 18
+let name ="gaurav"
+let state = null
+let isLogged = false
+let undefined;
+let h= null;
+console.log(typeof h)
+
+
+//number => 2 to power 53;
+// bigint
+//symbol=> unique
+//object
