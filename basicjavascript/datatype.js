0115d8702cbebdac0816ffaacc5bb8a816905e25
Gauravkumar010 <134727092+Gauravkumar010@users.noreply.github.com>
2023-12-11 17:42:18 +0000

    primitive datatype

--- basicjavascript/datatype.js
+++ basicjavascript/datatype.js
@@ -1,8 +1,9 @@
 "Use strict";
 
-console.log(3+30);
+// console.log(3+30);
 
-console.log("Gaurav")
+// console.log("Gaurav")
+
 
 let age = 18
 let name ="gaurav"
@@ -10,10 +11,52 @@ let state = null
 let isLogged = false
 let undefined;
 let h= null;
-console.log(typeof h)
+// console.log(typeof h)
 
 
 //number => 2 to power 53;
 // bigint
 //symbol=> unique
 //object
+
+
+//  Primitive
+
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+const score=100;
+const scoreValue=100.2;
+
+const value=null
+
+const Id=Symbol('125')
+const anotherId=Symbol('125')
+
+// console.log(Id === anotherId);
+
+let bigValue=12346897897814564n
+
+
+// console.log(typeof bigValue)
+
+
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+let heros = ["Ironman", "Thor", "Hulk", "Loki"]
+
+let myobj={
+    name:"gaurav",
+    age:21,
+}
+
+const myfunction=function(){
+    console.log("Hello gaurav")
+}
+
+
+console.log(typeof myfunction)
+
